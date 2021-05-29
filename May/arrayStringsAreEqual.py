e4cb732e44ce1288314e04e209fd6ce5ec0e9c58
Visakh47 <visakhbobby47@gmail.com>
2021-05-29 14:51:16 +0530

    update 29/05

--- May/arrayStringsAreEqual.py
+++ May/arrayStringsAreEqual.py
@@ -1,13 +1,13 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        finalw1 = ''
-        finalw2 = ''
+        w1 = ''
+        w2 = ''
         #try indexing the array -> faster
         for s in word1:
-            finalw1 = finalw1 +s
+            w1 = w1 +s
         for q in word2:
-            finalw2 =  finalw2 +q
-        if(finalw1==finalw2):
+            w2 =  w2 +q
+        if(w1==w2):
             return True 
         else:
             return False
