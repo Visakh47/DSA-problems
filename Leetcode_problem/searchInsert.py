a87d41346903c5330c2b3289606921905566c1ea
Visakh47 <visakhbobby47@gmail.com>
2021-06-25 15:34:06 +0530

    update 25/06

--- Leetcode_problem/searchInsert.py
+++ Leetcode_problem/searchInsert.py
@@ -10,3 +10,14 @@ class Solution:
                 else:
                     return pos
             return pos
+
+
+# Another Approach
+
+# class Solution:
+#     def searchInsert(self, nums: List[int], target: int) -> int:
+#         if(target in nums):
+#             return nums.index(target)
+#         nums.append(target)
+#         nums.sort()
+#         return nums.index(target)
