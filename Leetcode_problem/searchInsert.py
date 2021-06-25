02410edf2b321e8fe9f62f4653ae7084cfececfe
Visakh47 <visakhbobby47@gmail.com>
2021-06-25 15:33:22 +0530

    update 25/06

---
+++ Leetcode_problem/searchInsert.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            pos = 0
+            for i in range(len(nums)):
+                if (target>nums[i]):
+                    pos = pos + 1
+                else:
+                    return pos
+            return pos
