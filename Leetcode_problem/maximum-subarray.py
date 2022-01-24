acbdf8758a46313e293e0913a23a5f48f77ff706
Visakh <visakhbobby47@gmail.com>
2022-01-24 19:36:59 +0530

    kadane algo

--- Leetcode_problem/maximum-subarray.py
+++ Leetcode_problem/maximum-subarray.py
@@ -1,14 +1,24 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        mx = sum(nums)
-        n = len(nums)
-        sums = []
+#         mx = sum(nums)
+#         n = len(nums)
+#         sums = []
         
-        for i in range(n+1):
-            for j in range(i+1,n+1):
-                mx = max(mx,sum(nums[i:j]))
+#         for i in range(n+1):
+#             for j in range(i+1,n+1):
+#                 mx = max(mx,sum(nums[i:j]))
         
-        return mx
+#         return mx
+    
+        #implementing kadane's algorithm
+        current_max = nums[0]
+        highest_max = nums[0]
+        
+        for i in range(1,len(nums)):
+            current_max = max(nums[i],current_max+nums[i])
+            highest_max = max(current_max,highest_max)
+        
+        return highest_max
         
                             
                 
