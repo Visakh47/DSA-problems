294c91f64e285d059d43599d221f8afe0604ce65
Visakh <visakhbobby47@gmail.com>
2022-01-25 19:23:26 +0530

    update 25/01

--- Leetcode_problem/palindrome-ll.py
+++ Leetcode_problem/palindrome-ll.py
@@ -4,11 +4,51 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        #bruteforce
+#         node = head
+#         s = []
+#         while(node):
+#             s.append(node.val)
+#             node = node.next
+#         print(s)
+#         n = len(s)
+#         i = 0
+#         while( i< n//2 and n!=0 ):
+#             if(s[i]!=s[n-1-i]):
+#                 return False
+#             else:
+#                 i = i+1
         
-        str1 = []
-        while head:
-            str1.append(head.val)
-            head = head.next
+#         return True
+
+        #reverse the linked list but only till the midpoint given by slow
+
+        cur,prev,slow,fast = head,None,head,head
+        while(fast and fast.next):
+            #each time you break the link and give it back to prev
+            cur = slow # putting cur in next node 
+            slow = slow.next #slow is used for finding the midpoint and also next node
+            fast = fast.next.next
+            cur.next = prev # breaking link and giving it to prev
+            prev = cur
+            
+        #slow returns midpoint
         
-        return(str1[::-1]==str1)
+        #if the LL is odd,then fast will not be null
+        if(fast!=None):
+            slow = slow.next #we don't need to consider the middle node of odd list
+        
+        while(cur and slow): #cur is going in reverse, slow is going forward from middle
+            if(cur.val!=slow.val):
+                return False
+            cur = cur.next
+            slow = slow.next
+        return True
+        
+            
+            
+        
+        
+        
+        
